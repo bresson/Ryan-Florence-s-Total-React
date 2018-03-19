fca4983315b0850b92e09824733cff8b92867550
Chelsea Roman <chelsea.ann.roman@gmail.com>
2018-03-19 19:23:44 -0400

    implicit props and state passed to children--maybe can use for victory chart selection tabs

--- 02/02-exercise/src/App.js
+++ 02/02-exercise/src/App.js
@@ -6,11 +6,22 @@ import FaForward from 'react-icons/lib/fa/forward'
 import FaBackward from 'react-icons/lib/fa/backward'
 
 class RadioGroup extends Component {
+  state = {
+      value: this.props.defaultValue
+  }
   render() {
+    const children = React.Children.map(this.props.children, (child) =>{
+      return React.cloneElement(child, {
+        isActive: this.state.value === child.props.value,
+        onSelect: () => {
+          this.setState({ value: child.props.value })
+        }
+      }) //returns clone of child
+    }) //newProps aka implicit props
     return (
       <fieldset className="radio-group">
         <legend>{this.props.legend}</legend>
-        {this.props.children}
+        {children}
       </fieldset>
     )
   }
@@ -18,10 +29,11 @@ class RadioGroup extends Component {
 
 class RadioButton extends Component {
   render() {
-    const isActive = false // <-- should come from somewhere
+    const { isActive, onSelect } = this.props// <-- comes from children's state
     const className = 'radio-button ' + (isActive ? 'active' : '')
+    console.log(this.props)
     return (
-      <button className={className}>
+      <button className={className} onClick={onSelect}>
         {this.props.children}
       </button>
     )
@@ -32,7 +44,8 @@ class App extends Component {
   render() {
     return (
       <div>
-        <RadioGroup legend="Radio Group">
+        <RadioGroup defaultValue= "pause"
+          legend="Radio Group">
           <RadioButton value="back"><FaBackward/></RadioButton>
           <RadioButton value="play"><FaPlay/></RadioButton>
           <RadioButton value="pause"><FaPause/></RadioButton>
