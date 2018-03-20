d0cc78586875c8796c0d41de14fb84f82fc66829
Chelsea Roman <chelsea.ann.roman@gmail.com>
2018-03-20 13:38:19 -0400

    Completed exercise 4 with notes

--- 04/04-exercise/src/App.js
+++ 04/04-exercise/src/App.js
@@ -1,3 +1,5 @@
+//At some point he brings up hoisting and you don't know hoisting
+//and static classes... what are those?
 
 /*
 Create a `withStorage` higher order component that manages saving and retrieving
@@ -5,29 +7,41 @@ the `sidebarIsOpen` state to local storage
 */
 
 import './index.css'
-import React from 'react'
+import React, { Component } from 'react'
 import MenuIcon from 'react-icons/lib/md/menu'
 import { set, get, subscribe } from './local-storage'
 
-class App extends React.Component {
-  state = {
-    sidebarIsOpen: get('sidebarIsOpen', true)
-  }
-
-  componentDidMount() {
-    this.unsubscribe = subscribe(() => {
-      this.setState({
-        sidebarIsOpen: get('sidebarIsOpen')
+const withStorage = (storageKey, deflt) => (Comp) => { //be careful not to use the reserved word
+  return class withStorage extends Component {
+    state = {
+      [storageKey]: get(storageKey, true)
+    }
+//storageKey in [] is a dynamic variable because JS
+    componentDidMount() {
+      this.unsubscribe = subscribe(() => {
+        this.setState({
+          [storageKey]: get(storageKey)
+        })
       })
-    })
-  }
+    }
 
-  componentWillUnmount() {
-    this.unsubscribe()
+    componentWillUnmount() {
+      this.unsubscribe()
+    }
+    set = (value) => {
+      set(storageKey, value)
+    }
+    render(){
+      return <Comp {...this.state} setStorage={this.set} />
+    }
   }
+}
+class App extends React.Component {
 
   render() {
-    const { sidebarIsOpen } = this.state
+    const { sidebarIsOpen, setStorage } = this.state
+    //often you get state as a prop and some sort of function to
+    //manage that state
     return (
       <div className="app">
         <header>
@@ -35,7 +49,7 @@ class App extends React.Component {
             className="sidebar-toggle"
             title="Toggle menu"
             onClick={() => {
-              set('sidebarIsOpen', !sidebarIsOpen)
+              set(!sidebarIsOpen)
             }}
           >
             <MenuIcon/>
@@ -51,4 +65,8 @@ class App extends React.Component {
   }
 }
 
-export default App
+export default withStorage('sidebarIsOpen', true)(App)
+//getting sidebarisOpen from localstorage
+//default is true
+// withStorage('sidebarIsOpen', true) is a HOF that returns Comp
+//and then takes App and wraps it in behaviour stuff
